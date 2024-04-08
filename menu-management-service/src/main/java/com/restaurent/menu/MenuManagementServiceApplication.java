bed5b0c764dea143f455da94858317bf00b3ea60
Pasindu Lakshan <pasindu.lakshan@grubtech.com>
2024-04-08 19:57:08 +0530

    Create menu management service spring boot application

---
+++ menu-management-service/src/main/java/com/restaurent/menu/MenuManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restaurent.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuManagementServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MenuManagementServiceApplication.class, args);
+  }
+
+}
---
+++ menu-management-service/src/test/java/com/restaurent/menu/MenuManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurent.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuManagementServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
