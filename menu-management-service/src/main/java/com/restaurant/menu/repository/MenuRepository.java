29d1d67cc35ad6d15be42512ac40415c87d39eca
IT20026448 <it20026448@my.sliit.lk>
2024-04-19 08:04:20 +0530

    added comments and deleted .idea file

--- menu-management-service/src/main/java/com/restaurant/menu/repository/MenuRepository.java
+++ menu-management-service/src/main/java/com/restaurant/menu/repository/MenuRepository.java
@@ -5,6 +5,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+// This interface extends Spring Data MongoDB's MongoRepository to provide methods
+// for interacting with Menu data stored in a MongoDB database.
 @Repository
 public interface MenuRepository extends MongoRepository<Menu, ObjectId> {
 }
--- menu-management-service/src/main/java/com/restaurant/menu/service/MenuService.java
+++ menu-management-service/src/main/java/com/restaurant/menu/service/MenuService.java
@@ -11,24 +11,31 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import java.util.List;
 
+// This class provides services for managing the Menus.
 @Service
 @RequiredArgsConstructor
 @Log4j2
 public class MenuService {
-
+    // Injected dependency for interacting with Menu data in the database.
     private final MenuRepository menuRepository;
 
+    // Creates a new Menu object and saves it to the database using the injected MenuRepository which calls MongoRepository's save method..
     public Menu createMenu(Menu menu){
         return menuRepository.save(menu);
     }
 
+    // Retrieves all Menu objects from the database using the injected MenuRepository which calls MongoRepository's findAll method.
     public List<Menu> getAllMenus() { return menuRepository.findAll();
     }
 
+    // Retrieves a Menu object by its unique identifier (ObjectId) using the injected MenuRepository which calls MongoRepository's findById method and passes the menu id as a parameter.
     public Optional<Menu> getMenuById(ObjectId id) {
         return menuRepository.findById(id);
     }
 
+    // Updates an existing Menu object in the database using the injected MenuRepository.
+    // First finds the existing Menu by its id, then updates its fields based on the provided Menu object,
+    // and finally saves the updated Menu.
     public Menu updateMenu(Menu menu, ObjectId id) {
         // Find the existing menu by id
         Optional<Menu> existingMenu = menuRepository.findById(id);
@@ -46,6 +53,8 @@ public class MenuService {
         }
     }
 
+    // Deletes a Menu from the database based on its identifier (ObjectId) using the injected MenuRepository.
+    // Logs a message before deletion.
     public void deleteMenuById(ObjectId id){
         log.info("Deleting menu with id: {}", id);
         menuRepository.deleteById(id);
