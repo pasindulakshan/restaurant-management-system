8f03764c293f3843fd7267d94b80dfd5b1eb8075
Kalana-Sithum <kalanasithum1211@gmail.com>
2024-04-20 10:16:18 +0530

    Create MenuItemController.java

---
+++ menu-management-service/src/main/java/com/restaurant/menu/controller/MenuItemController.java
@@ -0,0 +1,74 @@
+package com.restaurant.menu.controller;
+import com.restaurant.menu.models.MenuItem;
+import com.restaurant.common.models.embeddable.Status;
+import com.restaurant.common.models.Audit;
+import com.restaurant.menu.service.MenuItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.bson.types.ObjectId;
+import java.util.List;
+import java.util.Optional;
+
+@RestController //class is a REST controller for handling API requests
+@RequestMapping(path = "/menuItems") //sets the base path for all the controller's endpoints "/menuItems"
+@RequiredArgsConstructor // This Lombok annotation reduces boilerplate code for constructor injection.
+@Log4j2
+public class MenuItemController {
+    private final MenuItemService menuItemService;
+
+    @PostMapping
+    public ResponseEntity<MenuItem> createMenuItem(@RequestBody MenuItem menuItem) {
+        MenuItem createdMenuItem = menuItemService.createMenuItem(menuItem);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMenuItem);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MenuItem> findMenuItemById(@PathVariable("id") String id) {
+        Optional<MenuItem> menuItemOptional = menuItemService.findMenuItemById(new ObjectId(id));
+        return menuItemOptional.map(menuItem -> ResponseEntity.ok().body(menuItem))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MenuItem>> findAllMenuItems() {
+        List<MenuItem> menuItems = menuItemService.findAllMenuItems();
+        return ResponseEntity.ok().body(menuItems);
+    }
+
+    @GetMapping("/notDeleted")
+    public ResponseEntity<List<MenuItem>> findAllNotDeletedMenuItems() {
+        List<MenuItem> menuItems = menuItemService.findAllNotDeletedMenuItems();
+        return ResponseEntity.ok().body(menuItems);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MenuItem> updateMenuItem(@PathVariable("id") String id, @RequestBody MenuItem updatedMenuItem) {
+        MenuItem menuItem = menuItemService.updateMenuItem(new ObjectId(id), updatedMenuItem);
+        if (menuItem != null) {
+            return ResponseEntity.ok().body(menuItem);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMenuItem(@PathVariable("id") String id) {
+        boolean deleted = menuItemService.deleteMenuItem(new ObjectId(id));
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
