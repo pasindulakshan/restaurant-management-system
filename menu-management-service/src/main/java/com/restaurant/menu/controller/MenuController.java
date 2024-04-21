d5a3394285fd3c8197e5b5e7317a885a69632f98
Amalya <it20254216@my.sliit.lk>
2024-04-22 01:22:18 +0530

    Catalog Srvice and controller completed

--- menu-management-service/src/main/java/com/restaurant/menu/controller/MenuController.java
+++ menu-management-service/src/main/java/com/restaurant/menu/controller/MenuController.java
@@ -7,7 +7,7 @@ import com.restaurant.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity; 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
